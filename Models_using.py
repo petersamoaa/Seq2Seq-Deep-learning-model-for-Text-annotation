5fbdf426647cffce96a7d6bdd9c46758545d9e36
Hazem Samoaa <37386593+hazemsamoaa@users.noreply.github.com>
2019-12-01 23:52:20 +0100

    Update Models_using.py

--- Models_using.py
+++ Models_using.py
@@ -5,8 +5,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from gensim.models import Word2Vec
 import pickle
-w2v_model = Word2Vec.load('C:\\Users\\Peter Samoaa\\Downloads\\results\\model.w2v')
-model = load_model('C:\\Users\\Peter Samoaa\\Downloads\\results\\model.h5')
+w2v_model = Word2Vec.load('C:\\Users\\Peter Samoaa\\Downloads\\results\\WordEmbedding.w2v')
+model = load_model('C:\\Users\\Peter Samoaa\\Downloads\\results\\TextProcessing_FeelingExtraction.h5')
 with open('C:\\Users\\Peter Samoaa\\Downloads\\results\\tokenizer.pkl', 'rb') as handle:
     tokenizer = pickle.load(handle)
 with open('C:\\Users\\Peter Samoaa\\Downloads\\results\\encoder.pkl', 'rb') as handle:
